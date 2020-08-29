544430b9016138a1e96fb81a055c663cd91d6f36
lokesh1199 <n0n4m399@tutanota.com>
2020-08-29 07:06:10 +0530

    Added the option to take the size as input

--- Pattern.java
+++ Pattern.java
@@ -3,10 +3,14 @@ import java.util.Scanner;
 public class Pattern {
     public static void main(String[] args) {
         int i, j;
-        for (i=1; i<=5; i++) {
-            for (j=i; j<=5; j++)
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        for (i=1; i<=n; i++) {
+            for (j=i; j<=n; j++)
                 System.out.print(j+" ");
-            for (j=1; j<i; j++)
+            for (j=i-1; j>0; j--)
                 System.out.print(j+" ");
             System.out.print("\n");
         }
